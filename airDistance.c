c6f2d1e48f8520ecd85f96cc9a4e086ed64a09f9
Nathan Hanzy <nhanzy2@huskers.unl.edu>
2020-08-31 00:56:28 +0000

    initial commit

---
+++ airDistance.c
@@ -0,0 +1,36 @@
+/**
+ * Author: Nathan Hanzy
+ * Date: 2020/08/27
+ *
+ * A program to calculate distance between two points
+ * when given 2 different longitudes and latitudes
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <math.h>
+
+ int main (int argc, char **argb){
+
+ double Dist;
+ double Lat1;
+ double Lat2;
+ double Lon1;
+ double Lon2;
+
+ printf("Enter Lat1:");
+ scanf("%lf", &Lat1);
+
+ printf("Enter Lon1:");
+ scanf("%lf" &Lon1);
+
+ printf("Enter Lat2:");
+ scanf("%lf" &Lat2);
+
+ printf("Enter Lon2:");
+ scanf("%lf" &Lon2);
+
+
+
+
+
+ }
