f338f622f3eb1c3503bac5c35c7d9f1d4851acdc
Nathan Hanzy <nhanzy2@huskers.unl.edu>
2020-09-04 20:05:17 +0000

    initial commit

--- airDistance.c
+++ airDistance.c
@@ -9,28 +9,36 @@
  #include <stdio.h>
  #include <math.h>
 
- int main (int argc, char **argb){
+ int main (int argc, char **argv) {
 
  double Dist;
  double Lat1;
  double Lat2;
  double Lon1;
  double Lon2;
+ 
 
  printf("Enter Lat1:");
  scanf("%lf", &Lat1);
 
  printf("Enter Lon1:");
- scanf("%lf" &Lon1);
+ scanf("%lf" , &Lon1);
 
  printf("Enter Lat2:");
- scanf("%lf" &Lat2);
+ scanf("%lf", &Lat2);
 
  printf("Enter Lon2:");
- scanf("%lf" &Lon2);
+ scanf("%lf", &Lon2);
+
+Lat1 = Lat1 /(180 / M_PI);
+Lat2 = Lat2 / (180 / M_PI);
+Lon1 = Lon1 / (180 / M_PI);
+Lon2 = Lon2 / (180 /M_PI);
 
 
+Dist = 6372 * (acos ( sin( Lat1 ) )* (sin (Lat2)) + (cos( Lat1 )) * (cos( Lat1)) * (Lon2 - Lon1));
 
+printf("%lf Dist" ,Dist);
 
-
+return 0;
  }
---
+++ cellPlan.c
@@ -0,0 +1,16 @@
+/**
+ *Author: Nathan Hanzy
+ * Date: 2020/09/03
+ * 
+ * A program to help cell phone customers 
+ * to help them understand if their using they're using too quickly
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main (int argc, char **argv){
+ 
+ 
+ 
+ 
+ }
