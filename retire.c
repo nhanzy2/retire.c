8223045768473eac08d96cbf20dd72f857f494f6
Bsudbeck <69834675+Bsudbeck@users.noreply.github.com>
2020-09-14 15:48:34 -0500

    Update retire.c

--- retire.c
+++ retire.c
@@ -8,6 +8,26 @@
  #include<stdio.h>
  
  int main (int argc, char **argv) {
+ int double startbBal,contributAmount, rateReturn, rateInflation;
+ int years;
+  
+  printf("initial starting balance:");
+  scanf("%lf", &startBal);
+ 
+  printf("monthly contribution amoount:");
+  scanf("%lf", &contributAmount);
+ 
+  printf("anual rate of return:");
+  scanf("%lf", &rateReturn);
+ 
+  printf("anual rate rate of inflation:");
+  scanf("%lf", &rateInflation);
+ 
+  printf("years until retirement:");
+  scanf("%d", &years);
+  
+  \
+ 
 
 
 
