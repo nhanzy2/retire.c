25027c761d36f4a928b590fec573aba49646c3d5
nhanzy2 <69830604+nhanzy2@users.noreply.github.com>
2020-09-14 20:22:53 -0500

    Update retire.c

--- retire.c
+++ retire.c
@@ -1,36 +1,32 @@
 /**
- * Author: Nathan Hanzy, Blake Sudbeck
+ * Author: Nathan Hanzy
  * Date: 2020/09/04
- * 
- * A program to calculate 401k
+ *
+ * A program to calculate 401k plan
  */
  #include<stdlib.h>
  #include<stdio.h>
- 
+
  int main (int argc, char **argv) {
- double startBal,contributAmount, rateReturn, rateInflation, interest, balance;
- int years, months;
-  
-  printf("initial starting balance:");
-  scanf("%lf", &startBal);
- 
-  printf("monthly contribution amoount:");
-  scanf("%lf", &contributAmount);
- 
-  printf("anual rate of return:");
-  scanf("%lf", &rateReturn);
- 
-  printf("anual rate rate of inflation:");
-  scanf("%lf", &rateInflation);
- 
-  printf("years until retirement:");
-  scanf("%d", &years);
+  double startBal, contributAmount, rateReturn, rateInflation, interest, balance;
+  int years;
+  int months;
+
+   startBal=atof(argv[1]);
+   contributAmount=atof(argv[2]);
+   rateReturn=atof(argv[3]);
+   rateInflation=atof(argv[4]);
+   years=atoi(argv[5]);
   
    interest=(startBal * (((1 + rateReturn)/(1 + rateInflation))-1))/12;
    balance= startBal + contributAmount + interest;
    months= years*12;
   
   printf("\nMonth    Interest    Balance\n");
+  
+  if(18500>=startBal){
+   printf("Error.");
+  }
 
   if(0>=rateReturn || rateReturn>=1){
    printf("Error.");
@@ -38,17 +34,18 @@
   if(0>=rateInflation || rateInflation>=1){
    printf("Error.");
   }
+  double monthlyIncrease;
 for(int month=1; month<=months; month++){
- printf("%d        %.2lf         %.2lf\n ", month, interest, balance);
- double monthlyIncrease= interest * (((1 + rateReturn)/(1 + rateInflation))-1);
+ printf("$%d        $%.2lf         $%.2lf\n ", month, interest, balance);
+ monthlyIncrease= interest * (((1 + rateReturn)/(1 + rateInflation))-1);
  interest= interest + monthlyIncrease;
  balance=balance + interest+ contributAmount;
- printf("total Nest Egg: "%lf, balance);
 }
 
- 
 
 
+   return 0;
+ }
 
 
 
